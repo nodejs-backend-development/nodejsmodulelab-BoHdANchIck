bf88ece414330a492f9787a6107ed949b898bc4c
BoHdANchIck <daneliuk.bohdan@chnu.edu.ua>
2025-03-21 16:05:02 +0200

    Refactor CSV processing and add CustomStream for input transformation

--- index.js
+++ index.js
@@ -3,32 +3,41 @@ const fs = require('fs');
 const url = require('url');
 const split2 = require('split2');
 const through2 = require('through2');
-const { Transform } = require('stream');
-
+const CustomStream = require('./CustomStream');
 
 const host = 'localhost';
 const port = 8000;
 
 const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
+
     // task5
     if (parsedUrl.pathname === '/task5') {
         let firstChunk = true;
+        let headers = []; 
+
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.write('[');
 
         const stream = fs.createReadStream('data.csv')
-            .pipe(split2())
-            .pipe(through2.obj((chunk, enc, callback) => {
-                const [name, age, city] = chunk.split(',');
-                const obj = JSON.stringify({ name, age, city });
-
-                if (!firstChunk) {
-                    callback(null, ',' + obj);
-                } else {
-                    firstChunk = false;
-                    callback(null, obj);
+            .pipe(split2()) 
+            .pipe(through2.obj(function (chunk, enc, callback) {
+                const row = chunk.split(',').map(val => val.trim());
+                if (!headers.length) {
+                    headers = row; 
+                    return callback(); 
                 }
+
+                const obj = row.reduce((acc, val, index) => {
+                    acc[headers[index]] = val;
+                    return acc;
+                }, {});
+
+                if (!firstChunk) this.push(',');
+                firstChunk = false;
+
+                this.push(JSON.stringify(obj));
+                callback();
             }));
 
         stream.on('data', (data) => {
@@ -43,7 +52,7 @@ const server = http.createServer((req, res) => {
             res.writeHead(500, { 'Content-Type': 'text/plain' });
             res.end('Error reading or processing file');
         });
-    } 
+    }
 
     // task3
     else if (parsedUrl.pathname === '/task3') {
@@ -53,10 +62,6 @@ const server = http.createServer((req, res) => {
             acc[key.trim()] = value;
             return acc;
         }, {});
-
-        // if (!cookieObj.user_info) {
-        //     res.setHeader('Set-Cookie', 'user_info=user1; Path=/; HttpOnly');
-        // }
 
         if (cookieObj.user_info === 'user1') {
             res.writeHead(200, { 'Content-Type': 'application/json' });
@@ -69,24 +74,14 @@ const server = http.createServer((req, res) => {
             res.writeHead(200, { 'Content-Type': 'application/json' });
             res.end(JSON.stringify({}));
         }
-    } 
+    }
 
     // task6
     else if (parsedUrl.pathname === '/task6') {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
+        const customStream = new CustomStream();
+        process.stdin.pipe(customStream);
         res.end('Processing input from console. Check logs.');
-
-        process.stdin.pipe(new Transform({
-            transform(chunk, _, callback) {
-                const transformed = chunk.toString().replace(/[a-z]/g, char => char.toUpperCase());
-                console.log(transformed);
-                callback();
-            }
-        }));
-    } 
-    else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Route not found');
     }
 });
 
